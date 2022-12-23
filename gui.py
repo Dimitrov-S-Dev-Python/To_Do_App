472806a681097f115ac481eee4d41d89a17a425f
python-sd <dimitrov.s.dev@gmail.com>
2022-12-24 01:24:54 +0200

    Implemented the Edit Button

--- gui.py
+++ gui.py
@@ -34,7 +34,7 @@ while True:
 			functions.write_todos(todos)
 			window['todos'].update(values=todos)
 		case "todos":
-			window['todo'].update(values=values['todos'][0])
+			window['todo'].update(value=values['todos'][0])
 		case sg.WIN_CLOSED:
 			break
 
